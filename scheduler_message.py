01069fe8ab7c6b1880bff102c2d31c55172233db
akahard2dj <akahard2dj@naver.com>
2017-10-08 21:29:58 +0900

    Update scheduler_message.py

--- scheduler_message.py
+++ scheduler_message.py
@@ -100,10 +100,6 @@ def timed_daily_report():
     telegram_sender.send_message(gpu_status_text, verbose=True)
     telegram_sender.send_message(mph_dashboard_text, verbose=True)
     telegram_sender.send_message(whattomine_report_text, verbose=True)
-    
-@sched.scheduled_job('interval', seconds=10)
-def deploy_test():
-    telegram_sender.send_message('test')
 
 @sched.scheduled_job('interval', minutes=10)
 def timed_warning_message():
