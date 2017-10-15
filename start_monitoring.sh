08af9136a5d5872c1aff6f2d711bdfcf62797e5a
Dowon-raspberrypi <akahard2dj@naver.com>
2017-10-15 22:17:57 +0900

    deploying bash update

--- start_monitoring.sh
+++ start_monitoring.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-echo "updating a .profile"
-source /home/pi/.profile
+echo "updating a .profile";
+source /home/pi/.profile;
 
-echo "Killing previous processes"
-/home/pi/Workspace/github/TelegramMinerMonitor/kill_screen.sh
+echo "Killing previous processes";
+/home/pi/Workspace/github/TelegramMinerMonitor/kill_screen.sh;
 
-echo "Scheduler job is starting"
-screen -d -m -S sched_job /home/pi/Workspace/github/TelegramMinerMonitor/sched_job.sh
+echo "Scheduler job is starting";
+screen -d -m -S sched_job /home/pi/Workspace/github/TelegramMinerMonitor/sched_job.sh;
 
-echo "Telegram bot is starting"
-screen -d -m -S miner_bot /home/pi/Workspace/github/TelegramMinerMonitor/miner_bot.sh
+echo "Telegram bot is starting";
+screen -d -m -S miner_bot /home/pi/Workspace/github/TelegramMinerMonitor/miner_bot.sh;
